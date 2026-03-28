383db5fa631795090c3d08b677debf4bda5edc37
ann-yurkevych <annyurkk@gmail.com>
2026-03-28 17:14:55 +0100

    change in configurations: add different hyper parameters

--- Portuguese client term deposit subscription/configurations.py
+++ Portuguese client term deposit subscription/configurations.py
@@ -1,12 +1,13 @@
 
 # 6 models: Decision Tree, K-Nearest Neigbors Classifier, Logistic Regression, XGBoost, CatBoost, LGBMBoost
 # 2 tuning hyper parameters methods: Randomized Search CV, Hyperopt Bayesian Optimization
-
-params_grid_search = {
+# as result: 12 tuned models - find the best model: model type, hyperparameters, method
+params_search = {
     "DecisionTree": {
         "model__max_depth": [3, 5, 10, None],
         "model__min_samples_split": [2, 5, 10],
         "model__min_samples_leaf": [1, 2, 4],
+        "model__criterion": ["gini", "entropy"]
     },
     "KNN": {
         "model__n_neighbors": range(1, 20),
@@ -14,23 +15,31 @@ params_grid_search = {
         "model__metric": ["euclidean", "manhattan"],
     },
     "LogisticRegression": {
-        "model__n_neighbors": range(1, 20),
-        "model__weights": ["uniform", "distance"],
-        "model__metric": ["euclidean", "manhattan"],
+        "model__penalty": ["l1", "l2", "elasticnet"],
+        "model__C": [0.01, 0.1, 1, 10, 100],
+        "model__solver": ["liblinear"],
+        "model__l1_ratio": [0, 0.5, 1],
     },
     
     "XGBoost": {
-        "model__n_estimators": [100, 200],
-        "model__max_depth": [3, 5],
+        "model__n_estimators": [100, 200, 500],
+        "model__max_depth": [3, 5, 7],
+        "model__learning_rate": [0.01, 0.1, 0.2],
+        "model__subsample": [0.7, 0.8, 1.0],
+        "model__colsample_bytree": [0.7, 0.8, 1.0],
     },
 
     "CatBoostClassifier": {
-        "model__n_estimators": [100, 200],
-        "model__max_depth": [3, 5],
+        "model__iterations": [100, 200, 500],
+        "model__depth": [4, 6, 8],
+        "model__learning_rate": [0.01, 0.1],
     },
 
     "LGBMClassifier": {
-        "model__n_estimators": [100, 200],
-        "model__max_depth": [3, 5],
+        "model__n_estimators": [100, 200, 500],
+        "model__max_depth": [-1, 3, 5, 7],
+        "model__learning_rate": [0.01, 0.1],
+        "model__num_leaves": [31, 50, 100],
     },
+
 }
